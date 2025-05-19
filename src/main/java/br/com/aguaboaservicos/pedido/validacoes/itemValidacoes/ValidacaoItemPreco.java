c765e4793a7155e11e99b9ce0e0bbd3816ba5362
JoelMNeto <joelneto441@gmail.com>
2025-05-19 15:04:14 -0300

    fix: arruma validação do item do pedido

--- src/main/java/br/com/aguaboaservicos/pedido/validacoes/itemValidacoes/ValidacaoItemPreco.java
+++ src/main/java/br/com/aguaboaservicos/pedido/validacoes/itemValidacoes/ValidacaoItemPreco.java
@@ -16,7 +16,7 @@ public class ValidacaoItemPreco implements ValidacaoItemPedido {
     public void validar(ItemPedidoCadastro item) {
         var produto = produtoRepository.findById(item.produtoId()).orElseThrow(EntityNotFoundException::new);
 
-        if (produto.getPreco().compareTo(item.precoUnitario()) < 0) {
+        if (produto.getPreco().compareTo(item.precoUnitario()) > 0) {
             throw new RuntimeException(
                     "O preço do item não pode ser menor do que o preço do produto! " + "Produto: " + produto.getId() +
                             " - " + produto.getNome());
